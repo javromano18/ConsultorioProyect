b2b36ff407e8e90f75534233361c296eb295e47a
javromano18 <romanojavierivan2020@gmail.com>
2026-04-13 12:08:10 -0300

    feat (backend): configurar proyecto inicial de Spring boot

---
+++ Backend/consultorio-proyect/src/main/java/com/PortafolioProyect/consultorio_proyect/ConsultorioProyectApplication.java
@@ -0,0 +1,13 @@
+package com.PortafolioProyect.consultorio_proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioProyectApplication.class, args);
+	}
+
+}
---
+++ Backend/consultorio-proyect/src/test/java/com/PortafolioProyect/consultorio_proyect/ConsultorioProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PortafolioProyect.consultorio_proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

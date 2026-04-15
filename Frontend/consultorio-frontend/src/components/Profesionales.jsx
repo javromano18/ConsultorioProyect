bb771fcf309b1f2afc4fcfd95a2f7c3971b76fad
javromano18 <romanojavierivan2020@gmail.com>
2026-04-15 01:08:38 -0300

    style (frontend): aplicar estilos al formulario de profesionales

--- Frontend/consultorio-frontend/src/components/Profesionales.jsx
+++ Frontend/consultorio-frontend/src/components/Profesionales.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
-
+import "../styles/Profesionales.css";
 
 function Profesionales() {
   const [profesionales, setProfesionales] = useState([]);

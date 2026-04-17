cf306bb6781e3aebd24d4f9535377bb56d77d200
javromano18 <romanojavierivan2020@gmail.com>
2026-04-17 11:39:16 -0300

    chore (frontend): mover Pacientes, Profesionales y Turnos a carpeta pages

--- Frontend/consultorio-frontend/src/App.jsx
+++ Frontend/consultorio-frontend/src/App.jsx
@@ -1,15 +1,24 @@
-import Pacientes from "./components/Pacientes";
-import Profesionales from "./components/Profesionales";
-import Turnos from "./components/Turnos"; 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Pacientes from "./pages/Pacientes";
+import Profesionales from "./pages/Profesionales";
+import Turnos from "./pages/Turnos";
 
 function App() {
   return (
-    <div>
-      <h1>Consultorio Odontológico</h1>
-      <Pacientes />
-      <Profesionales />
-      <Turnos /> 
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/pacientes">Pacientes</Link></li>
+          <li><Link to="/profesionales">Profesionales</Link></li>
+          <li><Link to="/turnos">Turnos</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/pacientes" element={<Pacientes />} />
+        <Route path="/profesionales" element={<Profesionales />} />
+        <Route path="/turnos" element={<Turnos />} />
+      </Routes>
+    </Router>
   );
 }
 
